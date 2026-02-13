e93ca90a8e04db6c4d4c68360f56f26c02b3cc00
Van Huynh <vanister@users.noreply.github.com>
2026-02-13 17:17:50 +0000

    Changed padding to be p-4 all around

--- src/pages/onboarding/OnboardingFooter.tsx
+++ src/pages/onboarding/OnboardingFooter.tsx
@@ -7,7 +7,7 @@ type OnboardingFooterProps = {
 export function OnboardingFooter(props: OnboardingFooterProps) {
   return (
     <div className="fixed bottom-0 left-0 right-0 z-10 bg-background border-t border-default">
-      <div className="max-w-2xl w-full mx-auto px-4 py-6">{props.children}</div>
+      <div className="max-w-2xl w-full mx-auto p-4">{props.children}</div>
     </div>
   );
 }
